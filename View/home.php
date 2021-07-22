5c83e99f85ecd05c34d8490573fc8672e9212a84
Nishigaki2706 <cn34927nc@gmail.com>
2021-07-23 08:29:25 +0900

    ポップオーバー、ライクカウントの実装

--- View/home.php
+++ View/home.php
@@ -105,6 +105,12 @@ function convertToDayTimeAgo(string $datetime)
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo HOME_URL;?>view/css/style.css">
+    <!-- JS -->
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous" defer></script>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <!--いいねJS-->
+    <script src="<?php echo HOME_URL;?>View/js/like.js" defer></script>
     <title>ホーム画面/twitterクローン</title>
 </head>
 <body class="home">
@@ -118,7 +124,8 @@ function convertToDayTimeAgo(string $datetime)
                     <li class="nav-item"><a href="nortification.php" class="nav-link"><img src="<?php echo HOME_URL;?>/View/img/icon-notification.svg" alt=""></a></li>
                     <li class="nav-item"><a href="profile.php" class="nav-link"><img src="<?php echo HOME_URL;?>/View/img/icon-profile.svg" alt=""></a></li>
                     <li class="nav-item"><a href="post.php" class="nav-link"><img src="<?php echo HOME_URL;?>/View/img/icon-post-tweet-twitterblue.svg" alt="" class="post-tweet"></a></li>
-                    <li class="nav-item my-icon"><img src="<?php echo HOME_URL;?>/View/img_uploaded/user/sample-person.jpg" alt="" class=""></li>
+                    <li class="nav-item my-icon"><img src="<?php echo HOME_URL;?>/View/img_uploaded/user/sample-person.jpg" alt="" class="js-popover" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right"data-bs-content="<a href='pofile.php'>プロフィール</a><br><a href='sign=out.php'>ログアウト</a>" data-bs-html="true"></li>
+                    
                 </ul>
             </div>
         </div>
@@ -170,7 +177,7 @@ function convertToDayTimeAgo(string $datetime)
                             <?php endif;?>    
 
                             <div class="icon-list">
-                                <div class="like">
+                                <div class="like js-Like" data-like-id="<?php echo htmlspecialchars($view_tweets['like_id ']);?>">
                                     <?php
                                     if (isset($view_tweets['like_id '])){
                                         ///いいねの場合
@@ -180,7 +187,7 @@ function convertToDayTimeAgo(string $datetime)
                                     }
                                     ?>
                                 </div>
-                                <div class="like-count"><?php echo htmlspecialchars($view_tweets['like_count']); ?></div>
+                                <div class="like-count js-like-count"><?php echo htmlspecialchars($view_tweets['like_count']); ?></div>
                             </div>
                         </div>
                     </div>
@@ -191,6 +198,14 @@ function convertToDayTimeAgo(string $datetime)
 
         </div>
     </div>
+    <script>
+        
+        document.addEventListener('DOMContentLoaded',function(){
+            $('.js-popover').popover({
+                container:'body'
+            })
+        },false);
+    </script>
 </body>
 
 </html>
