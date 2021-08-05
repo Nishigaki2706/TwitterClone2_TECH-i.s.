806fb0d52789f1263086f6b84d56fa038f88897e
Nishigaki2706 <cn34927nc@gmail.com>
2021-08-05 09:16:06 +0900

    モーダル,フォロー外す,する　の作成

--- View/profile.php
+++ View/profile.php
@@ -43,7 +43,52 @@
             <div class="profile-area">
                 <div class="top">
                     <div class="user"><img src="../View/img_uploaded/user/sample-person.jpg" alt=""></div>
-                    <button class="btn btn-reverse btn-sm">プロフィール編集</button>
+
+                    <?php if (isset($_GET['user_id'])) : ?>
+                        <!-- 相手のページ -->
+                        <?php if(isset($_GET['case'])): ?>
+                            <button class="btn btn-sm">フォローを外す</button>   
+                        <?php else: ?>
+                            <button class="btn btn-sm btn-reverse">フォローする</button>
+                        <?php endif; ?>
+                    <?php else: ?>
+                        <!-- 自分のページ -->
+                        <button class="btn btn-reverse btn-sm" data-bs-toggle="modal" data-bs-target="#js-modal">プロフィール編集</button>
+                    <?php endif; ?>
+
+                    <div class="modal fade" id="js-modal" tabindex="-1" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <form action="profile.php" method="post" enctype="multipart/form-data">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">プロフィールを編集</h5>
+                                        <botton class="btn-close" typd="button" data-bs-dismiss="modal" aria-label="Close"></botton>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="user">
+                                            <img src="../View/img_uploaded/user/sample-person.jpg" alt="">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="" class="mb-1">プロフィール写真</label>
+                                            <input type="file" class="form-control form-control-sm" name="image">
+                                        </div>
+
+                                        
+                                        <input type="text" class="form-control mb-4" name="nickname" value="太郎" placeholder="ニックネーム" maxlength="50" required >
+                                        <input type="text" class="form-control mb-4" name="name" value="taro" placeholder="ユーザー名" maxlength="50" required >
+                                        <input type="email" class="form-control mb-4" name="email" value="taro@techis.jp" placeholder="メールアドレス" maxlength="254" required >
+                                        <input type="password" class="form-control mb-4" name="password" value="" placeholder="パスワードを変更する場合はご入力ください" minlength="4" maxlength="128">
+                                    </div>
+
+                                    <div class="modal-footer">
+                                        <button class="btn btn-reverse" data-bs-dismiss="modal">キャンセル</button>
+                                        <button class="btn" type="submit">保存する</button>
+                                    </div>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="name">太郎</div>
