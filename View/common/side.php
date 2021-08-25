55ef453b1649d1fb6d3a79ce40983ab1ac6ca549
Nishigaki2706 <cn34927nc@gmail.com>
2021-08-25 11:45:39 +0900

    通知画面の作成

--- View/common/side.php
+++ View/common/side.php
@@ -4,7 +4,7 @@
                     <li class="nav-item"><a href="home.php" class="nav-link"><img src="<?php echo HOME_URL;?>/View/img/logo-twitterblue.svg" alt="" class="icon"></a></li>
                     <li class="nav-item"><a href="home.php" class="nav-link"><img src="<?php echo HOME_URL;?>/View/img/icon-home.svg" alt=""></a></li>
                     <li class="nav-item"><a href="search.php" class="nav-link"><img src="<?php echo HOME_URL;?>/View/img/icon-search.svg" alt=""></a></li>
-                    <li class="nav-item"><a href="nortification.php" class="nav-link"><img src="<?php echo HOME_URL;?>/View/img/icon-notification.svg" alt=""></a></li>
+                    <li class="nav-item"><a href="notification.php" class="nav-link"><img src="<?php echo HOME_URL;?>/View/img/icon-notification.svg" alt=""></a></li>
                     <li class="nav-item"><a href="profile.php" class="nav-link"><img src="<?php echo HOME_URL;?>/View/img/icon-profile.svg" alt=""></a></li>
                     <li class="nav-item"><a href="post.php" class="nav-link"><img src="<?php echo HOME_URL;?>/View/img/icon-post-tweet-twitterblue.svg" alt="" class="post-tweet"></a></li>
                     <li class="nav-item my-icon"><img src="<?php echo HOME_URL;?>/View/img_uploaded/user/sample-person.jpg" alt="" class="js-popover" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right"data-bs-content="<a href='pofile.php'>プロフィール</a><br><a href='sign=out.php'>ログアウト</a>" data-bs-html="true"></li>
