fe89be9f885717b7fd6eabe8b94c874e9232d98c
Nishigaki2706 <cn34927nc@gmail.com>
2021-10-20 12:04:25 +0900

    検索機能

---
+++ Controllers/search.php
@@ -0,0 +1,36 @@
+<?php
+//////////////////
+///サーチコントローラー
+//////////////////
+
+//設定を読み込み
+include_once '../config.php';
+
+//便利な関数を読み込み
+include_once '../util.php';
+
+//ツイートデータ操作モデルを読み込み
+include_once '../Models/tweets.php';
+
+//ログインチェック
+$user = getUserSession();
+if (!$user) {
+    //ログイン画面に遷移（ログインしていない）
+    header('Location: ' . HOME_URL . 'Controllers/sign-in.php');
+    exit;
+}
+
+//検索キーワードを取得
+$keyword = null;
+if (isset($_GET['keyword'])) {
+    $keyword = $_GET['keyword'];
+}
+//表示用の変数
+$view_user = $user;
+$view_keyword = $keyword;
+//ツイート一覧
+//TODO：モデルから取得する
+$view_tweets = findTweets($user,$keyword);
+
+//画面表示
+include_once '../View/search.php';
--- View/common/tweet.php
+++ View/common/tweet.php
@@ -11,11 +11,10 @@
                                     <span class="user-name">@<?php echo htmlspecialchars($view_tweet['user_name']); ?>・<?php echo convertToDayTimeAgo($view_tweet['tweet_created_at']); ?></span>
                                 </a>
                             </div>
-
-                            <p><?php echo htmlspecialchars($view_tweet['tweet_body']); ?></p>
+                            <p><?php echo $view_tweet['tweet_body']; ?></p>
 
                             <?php if(isset($view_tweet['tweet_image_name'])): ?>
-                                <img src="<?php echo buildImagePath ($view_tweet['tweet_image_name'],'tweet');?>" alt="" class="post-image">
+                                <img src="<?php echo buildImagePath ($view_tweet['tweet_image_name'],'tweet'); ?>" alt="" class="post-image">
                             <?php endif;?>    
 
                             <div class="icon-list">
